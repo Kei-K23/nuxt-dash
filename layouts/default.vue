ea444948399b24e7112be654dcb3f477eae01c16
Kei-K23 <keiksl2301@gmail.com>
2024-11-13 19:46:36 +0630

    chore: Init project and setup NuxtUI

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+    <footer>This is footer</footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const value = ref("");
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-500 text-xl">Welcome to the homepage</h1>
+    <UButton>Button</UButton>
+    <UCheckbox default-value />
+    <UInput v-model="value" />
+  </div>
+</template>
