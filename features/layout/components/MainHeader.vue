d10b28b7657ce1eba9bb781092592d19f4c45042
Kei-K23 <keiksl2301@gmail.com>
2024-11-14 19:34:24 +0630

    feat: add main header compoent to main layout

---
+++ features/layout/components/MainHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const search = ref("");
+</script>
+
+<template>
+  <header class="py-2 px-6 bg-white w-full flex items-center justify-between">
+    <div>
+      <UInput v-model="search" placeholder="search..." class="max-w-[350px]" />
+    </div>
+    <div class="flex items-center gap-x-5">
+      <div class="relative">
+        <UIcon name="i-heroicons-bell" class="size-6" />
+        <UBadge
+          class="absolute -top-1 -right-0.5 rounded-full size-4 flex items-center justify-center"
+          ><span class="text-xs">5</span></UBadge
+        >
+      </div>
+      <div class="flex items-center gap-x-2">
+        <UAvatar size="md" src="/img/user_avatar.png" alt="Avatar" />
+        <div class="flex flex-col">
+          <span>John Doe</span>
+          <span class="text-xs">Admin</span>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import MainHeader from "~/features/layout/components/MainHeader.vue";
 import Sidebar from "~/features/layout/components/Sidebar.vue";
 </script>
 
 <template>
-  <main class="h-screen flex bg-[#F5F6FA]">
+  <main class="h-screen w-full flex bg-[#F5F6FA]">
     <Sidebar />
-    <div>
+    <div class="flex-1">
+      <MainHeader />
       <slot />
     </div>
   </main>
