ccead7f8ba0d7721c2db2c9210c4e6a7601d1a27
Kei-K23 <keiksl2301@gmail.com>
2024-11-17 12:03:59 +0630

    feat: add create new member modal to create new member data

--- pages/team.vue
+++ pages/team.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { TEAM_MEMBERS } from "~/constants";
+import CreateNewMemberModal from "~/features/team/components/CreateNewMemberModal.vue";
 import TeamUserProfileCard from "~/features/team/components/TeamUserProfileCard.vue";
+import type { Member } from "~/types";
+
+let members = reactive<Member[]>(TEAM_MEMBERS);
 </script>
 
 <template>
   <section class="mt-5 px-6">
-    <h2 class="text-xl md:text-3xl font-bold">Team</h2>
+    <div class="flex items-center justify-between gap-x-4">
+      <h2 class="text-xl md:text-3xl font-bold">Team</h2>
+      <CreateNewMemberModal
+        @create-member="
+          (member) => {
+            members.push(member);
+          }
+        "
+      />
+    </div>
     <div class="mt-7 grid grid-cols-4 gap-x-5 gap-y-6">
       <TeamUserProfileCard
-        v-for="member in TEAM_MEMBERS"
+        v-for="member in members"
         :key="member.username"
         :member="member"
       />
