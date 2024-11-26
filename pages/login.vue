1677b33a3d8cda1d1d0a3034b60d85973c6d8a81
Kei-K23 <keiksl2301@gmail.com>
2024-11-26 18:45:01 +0630

    style: refactor the ui desing and layout for login and register pages

--- pages/login.vue
+++ pages/login.vue
@@ -26,58 +26,68 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 </script>
 
 <template>
-  <div
-    class="w-full h-screen flex flex-col items-center justify-center bg-emerald-500 dark:bg-emerald-700"
-  >
+  <div class="flex flex-col lg:flex-row w-full">
     <div
-      class="w-[320px] md:w-[450px] mx-auto border dark:border-gray-600 dark:shadow-gray-800 shadow-xl rounded-lg overflow-hidden p-6 bg-white dark:bg-neutral-800"
+      class="hidden lg:flex w-full lg:w-1/2 bg-emerald-500 dark:bg-emerald-700 items-center justify-center flex-col p-6"
     >
-      <h2 class="text-center text-2xl font-semibold">Login to Account</h2>
-      <p class="text-center text-sm dark:text-gray-300 text-gray-500">
-        Please enter your email and password to continue
-      </p>
-      <UForm
-        :schema="schema"
-        :state="state"
-        class="space-y-4 mt-6 mb-4"
-        @submit="onSubmit"
+      <h1 class="text-center text-white font-bold text-xl md:text-3xl mb-10">
+        VueDash - All in one Admin Dashboard
+      </h1>
+      <img src="/login.png" alt="login img" class="aspect-square" />
+    </div>
+    <div
+      class="w-full lg:w-1/2 h-screen flex flex-col items-center justify-center bg-emerald-500 dark:bg-emerald-700 lg:bg-white lg:dark:bg-black"
+    >
+      <div
+        class="w-[320px] md:w-[420px] lg:w-[480px] mx-auto overflow-hidden bg-white dark:bg-black lg:bg-none p-6 border lg:border-none shadow-xl lg:shadow-none rounded-md lg:rounded-none"
       >
-        <UFormGroup label="Email" name="email">
-          <UInput v-model="state.email" placeholder="example@gmail.com" />
-        </UFormGroup>
-
-        <UFormGroup label="Password" name="password">
-          <UInput
-            v-model="state.password"
-            type="password"
-            placeholder="pass123"
-          />
-        </UFormGroup>
-        <div class="flex items-center justify-between gap-x-4">
-          <UCheckbox
-            v-model="isRememberPassword"
-            name="rememberPassword"
-            label="Remember Password"
-          />
-          <ULink
-            to="/forget-password"
-            class="text-sm font-semibold hover:underline"
-            >Forget Password?</ULink
-          >
-        </div>
-
-        <UButton
-          type="submit"
-          class="w-full text-center flex items-center justify-center"
+        <h2 class="text-center text-2xl font-semibold">Login to Account</h2>
+        <p class="text-center text-sm dark:text-gray-300 text-gray-500">
+          Please enter your email and password to continue
+        </p>
+        <UForm
+          :schema="schema"
+          :state="state"
+          class="space-y-4 mt-6 mb-4"
+          @submit="onSubmit"
         >
-          Submit
-        </UButton>
-      </UForm>
-      <ULink
-        to="/register"
-        class="text-sm dark:text-gray-300 text-gray-500 hover:underline mt-4"
-        >Don't have an account? Register here</ULink
-      >
+          <UFormGroup label="Email" name="email">
+            <UInput v-model="state.email" placeholder="example@gmail.com" />
+          </UFormGroup>
+
+          <UFormGroup label="Password" name="password">
+            <UInput
+              v-model="state.password"
+              type="password"
+              placeholder="pass123"
+            />
+          </UFormGroup>
+          <div class="flex items-center justify-between gap-x-4">
+            <UCheckbox
+              v-model="isRememberPassword"
+              name="rememberPassword"
+              label="Remember Password"
+            />
+            <ULink
+              to="/forget-password"
+              class="text-sm font-semibold hover:underline"
+              >Forget Password?</ULink
+            >
+          </div>
+
+          <UButton
+            type="submit"
+            class="w-full text-center flex items-center justify-center"
+          >
+            Submit
+          </UButton>
+        </UForm>
+        <ULink
+          to="/register"
+          class="text-sm dark:text-gray-300 text-gray-500 hover:underline mt-4"
+          >Don't have an account? Register here</ULink
+        >
+      </div>
     </div>
   </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -30,63 +30,73 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 </script>
 
 <template>
-  <div
-    class="w-full h-screen flex flex-col items-center justify-center bg-emerald-500 dark:bg-emerald-700"
-  >
+  <div class="flex flex-col lg:flex-row w-full">
     <div
-      class="w-[320px] md:w-[450px] mx-auto border dark:border-gray-600 dark:shadow-gray-800 shadow-xl rounded-lg overflow-hidden p-6 bg-white dark:bg-neutral-800"
+      class="hidden lg:flex w-full lg:w-1/2 bg-emerald-500 dark:bg-emerald-700 items-center justify-center flex-col p-6"
     >
-      <h2 class="text-center text-2xl font-semibold">Create an Account</h2>
-      <p class="text-center text-sm dark:text-gray-300 text-gray-500">
-        Create a account to continue
-      </p>
-      <UForm
-        :schema="schema"
-        :state="state"
-        class="space-y-4 mt-6 mb-4 w-full"
-        @submit="onSubmit"
+      <h1 class="text-center text-white font-bold text-xl lg:text-3xl mb-10">
+        VueDash - All in one Admin Dashboard
+      </h1>
+      <img src="/register.png" alt="register img" class="aspect-square" />
+    </div>
+    <div
+      class="w-full lg:w-1/2 h-screen flex flex-col items-center justify-center bg-emerald-500 dark:bg-emerald-700 lg:bg-white lg:dark:bg-black"
+    >
+      <div
+        class="w-[320px] md:w-[420px] lg:w-[480px] mx-auto overflow-hidden bg-white dark:bg-black lg:bg-none p-6 border lg:border-none shadow-xl lg:shadow-none rounded-md lg:rounded-none"
       >
-        <UFormGroup label="Username" name="username">
-          <UInput v-model="state.username" placeholder="John Doe" />
-        </UFormGroup>
-        <UFormGroup label="Email" name="email">
-          <UInput v-model="state.email" placeholder="example@gmail.com" />
-        </UFormGroup>
-
-        <UFormGroup label="Password" name="password">
-          <UInput
-            v-model="state.password"
-            type="password"
-            placeholder="pass123"
-          />
-        </UFormGroup>
-        <UFormGroup label="Confirm Password" name="confirmPassword">
-          <UInput
-            v-model="state.confirmPassword"
-            type="password"
-            placeholder="pass123"
-          />
-        </UFormGroup>
-        <div class="flex">
-          <UCheckbox
-            v-model="isRememberPassword"
-            name="termsAndConditions"
-            label="I accept terms and conditions"
-          />
-        </div>
-
-        <UButton
-          type="submit"
-          class="w-full text-center flex items-center justify-center"
+        <h2 class="text-center text-2xl font-semibold">Create an Account</h2>
+        <p class="text-center text-sm dark:text-gray-300 text-gray-500">
+          Create a account to continue
+        </p>
+        <UForm
+          :schema="schema"
+          :state="state"
+          class="space-y-4 mt-6 mb-4 w-full"
+          @submit="onSubmit"
         >
-          Submit
-        </UButton>
-      </UForm>
-      <ULink
-        to="/login"
-        class="text-sm dark:text-gray-300 text-gray-500 hover:underline mt-4"
-        >Already have an account? Login here</ULink
-      >
+          <UFormGroup label="Username" name="username">
+            <UInput v-model="state.username" placeholder="John Doe" />
+          </UFormGroup>
+          <UFormGroup label="Email" name="email">
+            <UInput v-model="state.email" placeholder="example@gmail.com" />
+          </UFormGroup>
+
+          <UFormGroup label="Password" name="password">
+            <UInput
+              v-model="state.password"
+              type="password"
+              placeholder="pass123"
+            />
+          </UFormGroup>
+          <UFormGroup label="Confirm Password" name="confirmPassword">
+            <UInput
+              v-model="state.confirmPassword"
+              type="password"
+              placeholder="pass123"
+            />
+          </UFormGroup>
+          <div class="flex">
+            <UCheckbox
+              v-model="isRememberPassword"
+              name="termsAndConditions"
+              label="I accept terms and conditions"
+            />
+          </div>
+
+          <UButton
+            type="submit"
+            class="w-full text-center flex items-center justify-center"
+          >
+            Submit
+          </UButton>
+        </UForm>
+        <ULink
+          to="/login"
+          class="text-sm dark:text-gray-300 text-gray-500 hover:underline mt-4"
+          >Already have an account? Login here</ULink
+        >
+      </div>
     </div>
   </div>
 </template>
